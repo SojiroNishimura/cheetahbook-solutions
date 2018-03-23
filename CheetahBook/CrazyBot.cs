0a19f2ebfa5d65540fbf191262ea286b6c7038d1
snishimura <sjiro.nishimura@gmail.com>
2018-03-23 01:50:46 +0100

    Add solution : CrazyBot

--- CheetahBook/CrazyBot.cs
+++ CheetahBook/CrazyBot.cs
@@ -1,10 +1,38 @@
 ﻿using System;
+
 namespace CheetahBook
 {
-    public class CrazyBot
+    // SRM425
+    class CrazyBot
     {
-        public CrazyBot()
+        bool[,] grid = new bool[100, 100];
+        int[] dx = { 1, -1, 0, 0 };
+        int[] dy = { 0, 0, 1, -1 };
+        double[] probabilities = new double[4];
+
+        public double GetProbability(int n, int east, int west, int south, int north)
         {
+            probabilities[0] = east / 100d;
+            probabilities[1] = west / 100d;
+            probabilities[2] = south / 100d;
+            probabilities[3] = north / 100d;
+
+            return dfs(50, 50, n);
+        }
+
+        private double dfs(int x, int y, int n)
+        {
+            if (this.grid[x, y]) return 0d;
+            if (n == 0) return 1;
+            this.grid[x, y] = true;
+            var ret = 0.0;
+            for (var i = 0; i < 4; i++)
+            {
+                ret += dfs(x + dx[i], y + dy[i], n - 1) * probabilities[i];
+            }
+            this.grid[x, y] = false;
+            Console.WriteLine($"P:{ret} n:{n}");
+            return ret;
         }
     }
 }
--- CheetahBook/Program.cs
+++ CheetahBook/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Linq;
 
 namespace CheetahBook
 {
-    class MainClass
+    class Solution
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var reader = new System.IO.StreamReader("Inputs.txt");
+            Console.SetIn(reader);
+
+            var n = int.Parse(Console.ReadLine());
+            var directions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var bot = new CrazyBot();
+            var prob = bot.GetProbability(n, directions[0], directions[1], directions[2], directions[3]);
+            Console.WriteLine($"{prob}");
         }
     }
 }
